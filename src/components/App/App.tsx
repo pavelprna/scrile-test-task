72639739e5c8e80dae328aef29e804bff3beacf9
Pavel Zharov <pavel@prna.ru>
2021-12-16 17:02:43 +0700

    add users api class

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,15 @@
-import { ChangeEventHandler, FC, useState } from 'react'
+import { ChangeEventHandler, FC, useEffect, useState } from 'react'
+import { User } from '../../types'
+import { api } from '../../utils/Api'
 import SearchForm from '../SearchForm/SearchForm'
 
 const App: FC = () => {
   const [searchValue, setSearchValue] = useState<string>('')
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    api.getAllUsers().then((users) => setUsers(users))
+  })
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setSearchValue(e.target.value)
@@ -13,6 +20,7 @@ const App: FC = () => {
       <header className="header">
         <SearchForm value={searchValue} onChange={handleChange} />
       </header>
+      <main className="main"></main>
     </>
   )
 }
---
+++ src/utils/Api.ts
@@ -0,0 +1,34 @@
+import { User } from '../types'
+
+class Api {
+  baseUrl: string
+
+  constructor({ baseUrl }: { baseUrl: string }) {
+    this.baseUrl = baseUrl
+  }
+
+  _checkResponse(endpoint: string) {
+    return fetch(this.baseUrl + endpoint, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json;charset=utf-8' },
+    }).then((res) => {
+      if (res.ok) {
+        return res.json()
+      } else {
+        return Promise.reject()
+      }
+    })
+  }
+
+  getAllUsers(): Promise<User[]> {
+    return this._checkResponse('/users')
+  }
+
+  getAvatars() {
+    return this._checkResponse('/images')
+  }
+}
+
+export const api = new Api({
+  baseUrl: 'https://jsonplaceholder.typicode.com',
+})
